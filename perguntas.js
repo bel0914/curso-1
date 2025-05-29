2157e2225fb115467b20eb5a7bf935c32df43305
bel0914 <yetzibel.rodriguez@escola.pr.gov.br>
2025-05-29 09:18:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cultura general',
+    'Cuál es el país más grande del mundo',
+    'Rússia'
+)
+
+criaCartao(
+    ' Historia',
+    'Em que ano começou a Segunda Guerra Mundial?',
+    ' 1939'
+)
+
+criaCartao(
+    'Robótica',
+    'Quais são os 4 pilares da robótica?',
+    'sensores, atuadores, controle e inteligência artificial'
+)
+
+criaCartao(
+    'Conhecimento',
+    ' Quem inventou a lâmpada?',
+    'Thomas Edison'
+)
